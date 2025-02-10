6521de1b3d99da61874147399192bf511f13f216
Varun Mangalick <varun@xyla.com>
2025-02-09 22:00:32 -0500

    .

--- script.js
+++
@@ -1,8 +0,0 @@
-const face = document.getElementById("face");
-
-face.addEventListener('animationiteration', () => {
-    // Trigger dive animation after a certain number of bounces
-    if (face.style.animation.includes('bounce')) {
-        face.style.animation = 'bounce 2s infinite, dive 3s forwards';
-    }
-});
